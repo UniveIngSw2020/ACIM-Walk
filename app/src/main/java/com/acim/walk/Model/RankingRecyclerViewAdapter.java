092a58deee1d8090626331edeab89913a68af0f7
vittorioiamarino <iamarinovittorio68@gmail.com>
2020-12-13 19:25:46 +0100

    Implementazione classifica partita: 	- aggiunto file di layout ROW di ogni elemento della recycler view (ranking_recyclerview_row.xml) 	- implementato ViewAdapter, fa il binding dei dati alla recycler view (RankingRecyclerViewAdapter.java) 	- Deserializzazione collezione di oggetti provenienti da query Firebase (ShowRankingFragment.java)

---
+++ app/src/main/java/com/acim/walk/Model/RankingRecyclerViewAdapter.java
@@ -0,0 +1,104 @@
+package com.acim.walk.Model;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.acim.walk.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+
+/*
+ *
+ * this class BINDS some data to the RECYCLERVIEW of the SHOWRANKING fragment
+ *
+ */
+
+public class RankingRecyclerViewAdapter extends RecyclerView.Adapter<RankingRecyclerViewAdapter.ViewHolder> {
+
+    // this arraylist will store all the users queried from Firebase
+    private ArrayList<User> mData;
+    private LayoutInflater mInflater;
+
+
+    /**
+     *
+     * stores in mData the users queried from Firebase
+     *
+     * @param context
+     * @param data users queried from Firebase
+     */
+    public RankingRecyclerViewAdapter(Context context, ArrayList<User> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+
+    // factory method
+    @NonNull
+    @Override
+    public RankingRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.ranking_recyclerview_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+
+    // binds data to the textviews of each row of the recycler
+    @Override
+    public void onBindViewHolder(@NonNull RankingRecyclerViewAdapter.ViewHolder holder, int position) {
+
+        /*
+         * the @position param, is the CURRENT INDEX of the mData arraylist.
+         * mData[position] is basically users[i]
+         */
+
+        // getting some data
+        String username = mData.get(position).getEmail();
+        int steps = mData.get(position).getSteps();
+        int rankingPosition = position+1;
+
+        // displaying to the views
+        holder.usernameTxt.setText(username);
+        holder.stepsTxt.setText(String.valueOf(steps));
+        holder.rankingTxt.setText(String.valueOf(rankingPosition));
+    }
+
+
+    // factory method
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView rankingTxt;
+        TextView usernameTxt;
+        TextView stepsTxt;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            rankingTxt = itemView.findViewById(R.id.rankingPosition_txt);
+            usernameTxt = itemView.findViewById(R.id.rankingUsername_txt);
+            stepsTxt = itemView.findViewById(R.id.rankingSteps_txt);
+            //myTextView = itemView.findViewById(R.id.tvAnimalName);
+            //itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            //if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+
+}
--- app/src/main/java/com/acim/walk/ui/matchrecap/ShowRankingFragment.java
+++ app/src/main/java/com/acim/walk/ui/matchrecap/ShowRankingFragment.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.acim.walk.MainActivity;
+import com.acim.walk.Model.RankingRecyclerViewAdapter;
+import com.acim.walk.Model.User;
 import com.acim.walk.R;
+import com.acim.walk.Util;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
@@ -21,14 +27,29 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ShowRankingFragment extends Fragment {
 
     private String userId;
-
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    // recyclerview's adapter
+    private RankingRecyclerViewAdapter adapter;
+    /*
+     * this arraylist will contain the data (users) queried from Firebase.
+     * after being filled or updated, it will be binded to the recyclerview.
+     * the recyclerview will use the data of each element of the arraylist to fill each row
+     */
+    ArrayList<User> users = new ArrayList<>();
+
+
 
     public ShowRankingFragment() {
         // Required empty public constructor
@@ -37,8 +58,6 @@ public class ShowRankingFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
     }
 
     @Override
@@ -50,6 +69,20 @@ public class ShowRankingFragment extends Fragment {
         MainActivity main = (MainActivity) getActivity();
         userId = main.getUserID();
 
+        // setting up recyclerview
+        RecyclerView recyclerView = root.findViewById(R.id.rankingRecyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new RankingRecyclerViewAdapter(getContext(), users);
+        recyclerView.setAdapter(adapter);
+
+
+        /*
+         *
+         * querying Firebase:
+         * first we get the 'matchId' attribute from the 'users' collection to see the current match of the user
+         * then we use the 'matchId' to query all the participants of that match from the 'matches' collection
+         *
+         */
         Task<QuerySnapshot> userRes = db.collection("users")
                 .whereEqualTo("userId", userId)
                 .get();
@@ -57,6 +90,7 @@ public class ShowRankingFragment extends Fragment {
         userRes.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
                 if (task.isSuccessful()) {
                     for (QueryDocumentSnapshot document : task.getResult()) {
 
@@ -66,7 +100,63 @@ public class ShowRankingFragment extends Fragment {
                         // User is not playing any game
                         if (matchId == null) return;
 
-                        //TODO: implementare la parte di creazione classifica
+
+                        /*
+                         * at this point we have the matchId.
+                         * time to get all the participants of this match
+                         */
+                        Task<QuerySnapshot> matchRes = db.collection("matches")
+                                .whereEqualTo("matchId", matchId)
+                                .get();
+                        matchRes.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if(task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                        // deserializing [{}, .., {}] the collection of objects (users) from Firebase
+                                        List<Map<String, Object>> tempUsers = (List<Map<String, Object>>) document.get("participants");
+
+                                        // adding each user to the arraylist
+                                        for (Map<String, Object> user : tempUsers) {
+                                            int steps = Math.toIntExact((long) user.get("steps"));
+                                            String email = user.get("email").toString();
+                                            String userId = user.get("userId").toString();
+                                            String username = (user.get("username") == null) ? "Anonymous" : user.get("username").toString();
+
+                                            User temp = new User(email, userId, username, steps);
+                                            users.add(temp);
+                                        }
+
+                                        /*
+                                         * at this point the arraylist has ALL the participants of this match
+                                         * now we sort it (DESCENDING)
+                                         * user with the MOST steps is the FIRST
+                                         */
+                                        Collections.sort(users, new Comparator<User>() {
+                                            @Override
+                                            public int compare(User user, User t1) {
+                                                int mySteps = user.getSteps();
+                                                int opponentSteps = t1.getSteps();
+
+                                                if(mySteps > opponentSteps) return -1;
+                                                else if(mySteps < opponentSteps) return 1;
+                                                return 0;
+                                            }
+                                        });
+
+                                        /*
+                                         * we have all the participants of this match sorted by the number of steps
+                                         * we can now update the recyclerview
+                                         */
+                                        adapter.notifyDataSetChanged();
+
+                                    }
+                                } else {
+                                    // TODO: do something here to handle error
+                                }
+                            }
+                        });
 
 
                     }
