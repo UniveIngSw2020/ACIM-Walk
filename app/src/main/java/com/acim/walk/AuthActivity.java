cdc6c89119c89b8864ca593ddd6ae68e38f5fe5c
Riccardo Marostica <riccardo@itpowernet.it>
2021-01-04 20:07:15 +0100

    Modifiche grafiche
    Modifiche al menù e alla pagina di login

--- app/src/main/java/com/acim/walk/AuthActivity.java
+++ app/src/main/java/com/acim/walk/AuthActivity.java
@@ -1,35 +1,8 @@
 package com.acim.walk;
 
-import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.util.Log;
-import android.view.View;
-
-
-/*                            // taking logged user to MainActivity
-                            Intent myIntent = new Intent(getActivity(), MainActivity.class);
-                            // passing the Auth ID to MainActivity
-                            myIntent.putExtra("userID",user.getUid());
-                            // passing the user's email to MainActivity
-                            myIntent.putExtra("userEmail",user.getEmail());
-                            // passing the user's username to the MainActivity
-                            myIntent.putExtra("username", user.getDisplayName());
-                            // disabling animation for a better experience
-                            myIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                            // starting MainActivity
-                            getActivity().startActivity(myIntent);
- *
- * FirstFragment, SecondFramgment
- * activity_auth2, content_auth, fragment_first, fragment_second
- */
-
 
 public class AuthActivity extends AppCompatActivity {
 
@@ -40,9 +13,6 @@ public class AuthActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth2);
         Log.d(TAG, "AuthActivity --- onCreate");
-        //Intent myIntent = new Intent(AuthActivity.this, MainActivity.class);
-        //AuthActivity.this.startActivity(myIntent);
-
     }
 
     protected void onResume(){
