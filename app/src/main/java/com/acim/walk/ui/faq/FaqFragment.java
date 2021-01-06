cd77b664c257b909a60ab740496436b0c2da95d2
Esmir99 <875943@stud.unive.it>
2021-01-07 00:01:26 +0100

    impostazione FaqFragment
    aggiunto FAQ nel menù

---
+++ app/src/main/java/com/acim/walk/ui/faq/FaqFragment.java
@@ -0,0 +1,11 @@
+package com.acim.walk.ui.faq;
+
+import androidx.fragment.app.Fragment;
+
+public class FaqFragment extends Fragment {
+
+    private FaqViewModel faqViewModel;
+
+
+
+}
---
+++ app/src/main/java/com/acim/walk/ui/faq/FaqViewModel.java
@@ -0,0 +1,7 @@
+package com.acim.walk.ui.faq;
+
+import androidx.lifecycle.ViewModel;
+
+public class FaqViewModel extends ViewModel {
+    public FaqViewModel() {}
+}
